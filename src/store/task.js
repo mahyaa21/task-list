6c73bfcbce2f978648e33eaaa780c0945bd6a329
mahya <mahya@mahyas-MacBook-Pro.local>
2022-09-27 13:25:15 +0330

    feat: modify store

--- src/store/task.js
+++ src/store/task.js
@@ -1,19 +1,31 @@
-import { makeObservable, observable } from "mobx";
-
+import { action, makeObservable, observable } from "mobx";
 class TasksStore {
   label = "دیجی اکسپرس‌";
 
   // TODO - design the data model
-  tasks = undefined;
+  tasks = {
+    id: null,
+    title: null,
+    subTask: []
+  };
 
   constructor() {
     makeObservable(this, {
       label: observable,
       tasks: observable,
+      addTask: action
     });
   }
 
   // TODO - add needed methods to manipulate 'tasks'
+  addTask(id, task) {
+  }
+  
+  updateTask(id, property){
+  }
+  
+  removeTask(id){
+  }
 }
 
 export default new TasksStore();
